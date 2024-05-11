c521f6107e354b301419eddde508f90ceaedd7f2
TannerVoltageAlt <159682226+TannerVoltageAlt@users.noreply.github.com>
2024-05-11 13:16:03 -0700

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -3,14 +3,262 @@ namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
-
+            case "image1":
+            case "life":return img`
+. . . . . . . . . . . . . . . . 
+. . f f f f f f . f f f f f f . 
+. f f 3 3 3 3 f f f 3 3 3 3 f f 
+. f 3 3 3 3 3 3 f 3 3 3 3 3 3 f 
+. f 3 3 3 3 3 3 3 3 1 1 1 3 3 f 
+. f 3 3 3 3 3 3 3 3 1 1 1 3 3 f 
+. f 3 3 3 3 3 b b b 1 1 1 3 3 f 
+. f 3 3 3 3 b b b b b 3 3 3 3 f 
+. f f 3 3 b b b b b b b 3 3 f f 
+. . f f 3 b b b b b b b 3 f f . 
+. . . f f b b b b b b b f f . . 
+. . . . f f b b b b b f f . . . 
+. . . . . f f b b b f f . . . . 
+. . . . . . f f b f f . . . . . 
+. . . . . . . f f f . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "33XR^-Q{YnuL7SlQz*o;":
+            case "coin":return img`
+. . . . b b b b b b b b . . . . 
+. . . . b b b b b b b b . . . . 
+. . b b 5 5 5 5 5 5 5 5 b b . . 
+. . b b 5 5 5 5 5 5 5 5 b b . . 
+b b 5 5 d d 3 3 3 3 d d 5 5 b b 
+b b 5 5 d d 3 3 3 3 d d 5 5 b b 
+b b 5 5 3 3 5 5 5 5 1 1 5 5 b b 
+b b 5 5 3 3 5 5 5 5 1 1 5 5 b b 
+c c 5 5 3 3 5 5 5 5 1 1 d d c c 
+c c 5 5 3 3 5 5 5 5 1 1 d d c c 
+c c d d d d 1 1 1 1 d d d d c c 
+c c d d d d 1 1 1 1 d d d d c c 
+. . f f d d d d d d d d f f . . 
+. . f f d d d d d d d d f f . . 
+. . . . f f f f f f f f . . . . 
+. . . . f f f f f f f f . . . . 
+`;
+            case "image3":
+            case "goal":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . 6 6 6 6 . . . . . . 
+. . . . 6 6 6 5 5 6 6 6 . . . . 
+. . . 7 7 7 7 6 6 6 6 6 6 . . . 
+. . 6 7 7 7 7 8 8 8 1 1 6 6 . . 
+. . 7 7 7 7 7 8 8 8 1 1 5 6 . . 
+. 6 7 7 7 7 8 8 8 8 8 5 5 6 6 . 
+. 6 7 7 7 8 8 8 6 6 6 6 5 6 6 . 
+. 6 6 7 7 8 8 6 6 6 6 6 6 6 6 . 
+. 6 8 7 7 8 8 6 6 6 6 6 6 6 6 . 
+. . 6 8 7 7 8 6 6 6 6 6 8 6 . . 
+. . 6 8 8 7 8 8 6 6 6 8 6 6 . . 
+. . . 6 8 8 8 8 8 8 8 8 6 . . . 
+. . . . 6 6 8 8 8 8 6 6 . . . . 
+. . . . . . 6 6 6 6 . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "fail":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . 4 4 4 4 . . . . . . 
+. . . . 4 4 4 5 5 4 4 4 . . . . 
+. . . 3 3 3 3 4 4 4 4 4 4 . . . 
+. . 4 3 3 3 3 2 2 2 1 1 4 4 . . 
+. . 3 3 3 3 3 2 2 2 1 1 5 4 . . 
+. 4 3 3 3 3 2 2 2 2 2 5 5 4 4 . 
+. 4 3 3 3 2 2 2 4 4 4 4 5 4 4 . 
+. 4 4 3 3 2 2 4 4 4 4 4 4 4 4 . 
+. 4 2 3 3 2 2 4 4 4 4 4 4 4 4 . 
+. . 4 2 3 3 2 4 4 4 4 4 2 4 . . 
+. . 4 2 2 3 2 2 4 4 4 2 4 4 . . 
+. . . 4 2 2 2 2 2 2 2 2 4 . . . 
+. . . . 4 4 2 2 2 2 4 4 . . . . 
+. . . . . . 4 4 4 4 . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
         }
         return null;
     })
 
     helpers._registerFactory("animation", function(name: string) {
         switch(helpers.stringTrim(name)) {
-
+            case "lifeAnim":
+            case "anim1":return [img`
+. . . . . . . . . . . . . . . . 
+. . f f f f f f . f f f f f f . 
+. f f 3 3 3 3 f f f 3 3 3 3 f f 
+. f 3 3 3 3 3 3 f 3 3 3 3 3 3 f 
+. f 3 3 3 3 3 3 3 3 1 1 1 3 3 f 
+. f 3 3 3 3 3 3 3 3 1 1 1 3 3 f 
+. f 3 3 3 3 3 b b b 1 1 1 3 3 f 
+. f 3 3 3 3 b b b b b 3 3 3 3 f 
+. f f 3 3 b b b b b b b 3 3 f f 
+. . f f 3 b b b b b b b 3 f f . 
+. . . f f b b b b b b b f f . . 
+. . . . f f b b b b b f f . . . 
+. . . . . f f b b b f f . . . . 
+. . . . . . f f b f f . . . . . 
+. . . . . . . f f f . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . f f f f f . f f f f f . . 
+. . f f 3 3 3 f f f 3 3 3 f f . 
+. . f 3 3 3 3 3 f 3 3 3 3 3 f . 
+. . f 3 3 3 3 3 3 3 1 1 3 3 f . 
+. . f 3 3 3 3 3 3 3 1 1 3 3 f . 
+. . f 3 3 3 3 3 3 3 3 3 3 3 f . 
+. . f f 3 3 3 b b b 3 3 3 f f . 
+. . . f f 3 b b b b b 3 f f . . 
+. . . . f f b b b b b f f . . . 
+. . . . . f f b b b f f . . . . 
+. . . . . . f f b f f . . . . . 
+. . . . . . . f f f . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f f f . f f f . . . . 
+. . . . f 3 3 3 f 3 3 3 f . . . 
+. . . . f 3 3 3 3 3 1 3 f . . . 
+. . . . f 3 3 3 3 3 3 3 f . . . 
+. . . . . f 3 b b b 3 f . . . . 
+. . . . . f f b b b f f . . . . 
+. . . . . . f f b f f . . . . . 
+. . . . . . . f f f . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . f f f f f . f f f f f . . 
+. . f f 3 3 3 f f f 3 3 3 f f . 
+. . f 3 3 3 3 3 f 3 3 3 3 3 f . 
+. . f 3 3 3 3 3 3 3 1 1 3 3 f . 
+. . f 3 3 3 3 3 3 3 1 1 3 3 f . 
+. . f 3 3 3 3 3 3 3 3 3 3 3 f . 
+. . f f 3 3 3 b b b 3 3 3 f f . 
+. . . f f 3 b b b b b 3 f f . . 
+. . . . f f b b b b b f f . . . 
+. . . . . f f b b b f f . . . . 
+. . . . . . f f b f f . . . . . 
+. . . . . . . f f f . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`];
+            case "coinAnim":
+            case "=o6YQ;xYfQH#IlE/$|ag":return [img`
+. . . . b b b b b b b b . . . . 
+. . . . b b b b b b b b . . . . 
+. . b b 5 5 5 5 5 5 5 5 b b . . 
+. . b b 5 5 5 5 5 5 5 5 b b . . 
+b b 5 5 d d 3 3 3 3 d d 5 5 b b 
+b b 5 5 d d 3 3 3 3 d d 5 5 b b 
+b b 5 5 3 3 5 5 5 5 1 1 5 5 b b 
+b b 5 5 3 3 5 5 5 5 1 1 5 5 b b 
+c c 5 5 3 3 5 5 5 5 1 1 d d c c 
+c c 5 5 3 3 5 5 5 5 1 1 d d c c 
+c c d d d d 1 1 1 1 d d d d c c 
+c c d d d d 1 1 1 1 d d d d c c 
+. . f f d d d d d d d d f f . . 
+. . f f d d d d d d d d f f . . 
+. . . . f f f f f f f f . . . . 
+. . . . f f f f f f f f . . . . 
+`, img`
+. . . . b b b b b b . . . . . . 
+. . . . b b b b b b . . . . . . 
+. . b b 5 5 5 5 5 5 b b . . . . 
+. . b b 5 5 5 5 5 5 b b . . . . 
+b b 5 5 d d 3 3 d d 5 5 b b . . 
+b b 5 5 d d 3 3 d d 5 5 b b . . 
+b b 5 5 3 3 5 5 1 1 5 5 b b . . 
+b b 5 5 3 3 5 5 1 1 5 5 b b . . 
+c c 5 5 3 3 5 5 1 1 d d c c . . 
+c c 5 5 3 3 5 5 1 1 d d c c . . 
+c c 5 5 d d 1 1 d d d d c c . . 
+c c 5 5 d d 1 1 d d d d c c . . 
+. . f f d d d d d d f f . . . . 
+. . f f d d d d d d f f . . . . 
+. . . . f f f f f f . . . . . . 
+. . . . f f f f f f . . . . . . 
+`, img`
+. . . . . . b b b b . . . . . . 
+. . . . . . b b b b . . . . . . 
+. . . . b b 5 5 5 5 b b . . . . 
+. . . . b b 5 5 5 5 b b . . . . 
+. . b b 5 5 d d 1 1 5 5 b b . . 
+. . b b 5 5 d d 1 1 5 5 b b . . 
+. . b b 5 5 3 3 1 1 5 5 b b . . 
+. . b b 5 5 3 3 1 1 5 5 b b . . 
+. . c c 5 5 3 3 1 1 d d c c . . 
+. . c c 5 5 3 3 1 1 d d c c . . 
+. . c c 5 5 1 1 d d d d c c . . 
+. . c c 5 5 1 1 d d d d c c . . 
+. . . . f f d d d d f f . . . . 
+. . . . f f d d d d f f . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . . . f f f f . . . . . . 
+`, img`
+. . . . . . b b b b . . . . . . 
+. . . . . . b b b b . . . . . . 
+. . . . b b 5 5 5 5 b b . . . . 
+. . . . b b 5 5 5 5 b b . . . . 
+. . . . b b 1 1 1 1 b b . . . . 
+. . . . b b 1 1 1 1 b b . . . . 
+. . . . b b 5 5 5 5 b b . . . . 
+. . . . b b 5 5 5 5 b b . . . . 
+. . . . b b d d d d b b . . . . 
+. . . . b b d d d d b b . . . . 
+. . . . c c d d d d c c . . . . 
+. . . . c c d d d d c c . . . . 
+. . . . c c 3 3 3 3 c c . . . . 
+. . . . c c 3 3 3 3 c c . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . . . f f f f . . . . . . 
+`, img`
+. . . . . . b b b b . . . . . . 
+. . . . . . b b b b . . . . . . 
+. . . . b b 5 5 5 5 b b . . . . 
+. . . . b b 5 5 5 5 b b . . . . 
+. . b b 5 5 1 1 d d 5 5 b b . . 
+. . b b 5 5 1 1 d d 5 5 b b . . 
+. . b b 5 5 1 1 3 3 5 5 b b . . 
+. . b b 5 5 1 1 3 3 5 5 b b . . 
+. . c c d d 1 1 3 3 5 5 c c . . 
+. . c c d d 1 1 3 3 5 5 c c . . 
+. . c c d d d d 1 1 5 5 c c . . 
+. . c c d d d d 1 1 5 5 c c . . 
+. . . . f f d d d d f f . . . . 
+. . . . f f d d d d f f . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . . . f f f f . . . . . . 
+`, img`
+. . . . . . b b b b b b . . . . 
+. . . . . . b b b b b b . . . . 
+. . . . b b 5 5 5 5 5 5 b b . . 
+. . . . b b 5 5 5 5 5 5 b b . . 
+. . b b 5 5 d d 3 3 d d 5 5 b b 
+. . b b 5 5 d d 3 3 d d 5 5 b b 
+. . b b 5 5 1 1 5 5 3 3 5 5 b b 
+. . b b 5 5 1 1 5 5 3 3 5 5 b b 
+. . c c d d 1 1 5 5 3 3 5 5 c c 
+. . c c d d 1 1 5 5 3 3 5 5 c c 
+. . c c d d d d 1 1 d d 5 5 c c 
+. . c c d d d d 1 1 d d 5 5 c c 
+. . . . f f d d d d d d f f . . 
+. . . . f f d d d d d d f f . . 
+. . . . . . f f f f f f . . . . 
+. . . . . . f f f f f f . . . . 
+`];
         }
         return null;
     })
--- main.ts
+++ main.ts
@@ -1,7 +1,36 @@
 namespace SpriteKind {
     export const goalSpot = SpriteKind.create()
     export const failSpot = SpriteKind.create()
-    export const lifeSprite = SpriteKind.create()
+    export const lifeItem = SpriteKind.create()
+    export const coinItem = SpriteKind.create()
 }
-let mySprite = Render.getRenderSpriteVariable()
-let list = [tilemap`level0`]
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    Render.jumpWithHeightAndDuration(mySprite, 16, 500)
+})
+tileUtil.onMapLoaded(function (tilemap2) {
+    tileUtil.createSpritesOnTiles(assets.tile`coin`, assets.image`coin`, SpriteKind.coinItem)
+    tileUtil.createSpritesOnTiles(assets.tile`life`, assets.image`life`, SpriteKind.lifeItem)
+    tileUtil.createSpritesOnTiles(assets.tile`goal`, assets.image`goal`, SpriteKind.goalSpot)
+    tileUtil.createSpritesOnTiles(assets.tile`fail`, assets.image`fail`, SpriteKind.failSpot)
+})
+sprites.onCreated(SpriteKind.lifeItem, function (sprite) {
+    Render.setSpriteAnimations(sprite, Render.createAnimations(100, assets.animation`lifeAnim`))
+})
+controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
+    Render.toggleViewMode()
+})
+sprites.onCreated(SpriteKind.coinItem, function (sprite) {
+    Render.setSpriteAnimations(sprite, Render.createAnimations(100, assets.animation`coinAnim`))
+})
+let mySprite: Sprite = null
+mySprite = Render.getRenderSpriteVariable()
+let list = [
+tilemap`level0`,
+tilemap`level1`,
+tilemap`level3`,
+tilemap`level4`,
+tilemap`level6`,
+tilemap`level7`
+]
+tiles.setCurrentTilemap(list[0])
+info.setLife(3)
