d15c7465b1e9b8e149b76906edbd19b16048ca93
TannerVoltageAlt <159682226+TannerVoltageAlt@users.noreply.github.com>
2024-05-11 13:36:52 -0700

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -4,14 +4,37 @@ namespace SpriteKind {
     export const lifeItem = SpriteKind.create()
     export const coinItem = SpriteKind.create()
 }
+sprites.onOverlap(SpriteKind.Player, SpriteKind.lifeItem, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite, effects.hearts, 500)
+    info.changeLifeBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.goalSpot, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite)
+    tileUtil.unloadTilemap()
+    currentLevel += 1
+    if (currentLevel < 6) {
+        music.play(music.createSong(hex`00780004080200`), music.PlaybackMode.LoopingInBackground)
+        loadCurrentLevel()
+    } else {
+        game.gameOver(true)
+    }
+})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     Render.jumpWithHeightAndDuration(mySprite, 16, 500)
+})
+function loadCurrentLevel () {
+    tiles.setCurrentTilemap(list[currentLevel])
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.coinItem, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite, effects.rings, 500)
+    info.changeScoreBy(1)
 })
 tileUtil.onMapLoaded(function (tilemap2) {
     tileUtil.createSpritesOnTiles(assets.tile`coin`, assets.image`coin`, SpriteKind.coinItem)
     tileUtil.createSpritesOnTiles(assets.tile`life`, assets.image`life`, SpriteKind.lifeItem)
     tileUtil.createSpritesOnTiles(assets.tile`goal`, assets.image`goal`, SpriteKind.goalSpot)
     tileUtil.createSpritesOnTiles(assets.tile`fail`, assets.image`fail`, SpriteKind.failSpot)
+    tiles.placeOnRandomTile(mySprite, assets.tile`spawnTile`)
 })
 sprites.onCreated(SpriteKind.lifeItem, function (sprite) {
     Render.setSpriteAnimations(sprite, Render.createAnimations(100, assets.animation`lifeAnim`))
@@ -22,15 +45,18 @@ controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
 sprites.onCreated(SpriteKind.coinItem, function (sprite) {
     Render.setSpriteAnimations(sprite, Render.createAnimations(100, assets.animation`coinAnim`))
 })
+let currentLevel = 0
+let list: tiles.TileMapData[] = []
 let mySprite: Sprite = null
 mySprite = Render.getRenderSpriteVariable()
-let list = [
+list = [
 tilemap`level0`,
 tilemap`level1`,
+tilemap`level2`,
 tilemap`level3`,
 tilemap`level4`,
-tilemap`level6`,
-tilemap`level7`
+tilemap`level5`
 ]
-tiles.setCurrentTilemap(list[0])
+currentLevel = 0
 info.setLife(3)
+loadCurrentLevel()
